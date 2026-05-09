5aac6fc2e4c0fae1a4beaafbae1cf6617a7ff653
ahmedbettaieb11 <ahmed.bettaieb@etudiant-isi.utm.tn>
2026-05-09 19:38:12 +0200

    Update connexion.php

--- includes/connexion.php
+++ includes/connexion.php
@@ -1,6 +1,6 @@
 <?php
 // Connexion à la base de données
-$conn = mysqli_connect("127.0.0.1", "root", "", "kickstart_db", 3307);
+$conn = mysqli_connect("127.0.0.1", "root", "", "kickstart-db", 3307);
 
 if (!$conn) {
     die("Erreur de connexion : " . mysqli_connect_error());
